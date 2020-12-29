36067ce5901b0b099ef22f87066d57ed937e8fde
Andriy <abortnik08@gmail.com>
2020-12-29 13:27:31 -0800

    Adding another test commit

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Andriy Bortnik')
+    print_hi("Commit 2")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
